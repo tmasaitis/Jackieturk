8f292fbcdcaaaafc23a4f6ac12ba00621f0cff91
tmasaitis <lukas@masaitis.us>
2026-09-12 20:42:44 -0400

    Added PIDController class.

---
+++ src/pidController.hpp
@@ -0,0 +1,46 @@
+#ifndef PIDCONTROLLER
+#define PIDCONTROLLER
+
+#include <algorithm>
+
+class PIDController
+{
+public:
+
+  struct Parameters
+  {
+    double P = 0;
+    double I = 0;
+    double D = 0;
+    double limit = 1;
+  };
+
+private:
+  double accumulator = 0;
+  double previousError = 0;
+  Parameters parameters{};
+
+  double clamp(double x)
+  {
+    double limit = parameters.limit;
+    return std::max( std::min(x, limit), -limit );
+  }
+
+public:
+
+  PIDController(Parameters parameters): parameters{parameters} {}
+
+  double step(double error)
+  {
+    accumulator = clamp(accumulator + parameters.I * error);
+    double derivative = error - previousError;
+
+    double control = parameters.P * error + accumulator + parameters.D * derivative;
+    previousError = error;
+
+    return control;
+  }
+};
+
+
+#endif
